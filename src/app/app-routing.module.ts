15f1b5d3608ec868286aa0ed10b6564b43962336
xthiagosant <uzmaki_thiago@outlook.com>
2022-02-16 11:10:49 -0300

    Adicionado rota para cadastro de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ResumoComponent } from './resumo/resumo.component';
@@ -7,6 +8,7 @@ import { ResumoComponent } from './resumo/resumo.component';
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
+  {path: 'cadastro-usuario', component: CadastroUsuarioComponent},
   {path: 'resumo', component: ResumoComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
